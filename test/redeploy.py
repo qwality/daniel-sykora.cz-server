6e393de85269cee838bb3ebff40d73c776726b59
qwality <low.qwality@gmail.com>
2023-11-27 14:45:38 +0100

    dsdas

--- test/redeploy.py
+++ test/redeploy.py
@@ -3,7 +3,7 @@ import subprocess
 print('start pyhton')
 
 subprocess.run(['python', 'test/test_1/run.py'])
-subprocess.run(['python', 'test/test_1/stop.py'])
+# subprocess.run(['python', 'test/test_1/stop.py'])
 
 # print(p.pid)
 
