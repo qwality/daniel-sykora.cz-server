1d2439aa32e077c2d24a06a69db36d170d7a66e9
qwality <low.qwality@gmail.com>
2023-12-01 14:52:58 +0100

    path

--- scripts/server.py
+++ scripts/server.py
@@ -51,7 +51,7 @@ if args.self:
 else:
     for sub_dir_name in filter(lambda i: os.path.isdir(i), os.listdir(WEBS_PATH)):
         sub_dir_path = os.path.join(WEBS_PATH, sub_dir_name)
-        print(f'\tp: checking {sub_dir_name}')
+        print(f'\tp: checking {sub_dir_path}')
 
         if CFG_FILE in os.listdir(sub_dir_path) and (args.all or sub_dir_name in servers_to_config):
             print(f'\tp: updating {sub_dir_name}')
