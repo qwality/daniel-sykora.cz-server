4e1bcb92c7241769016b622810666612d44c71da
qwality <low.qwality@gmail.com>
2023-12-01 19:32:11 +0100

    trytryutr

--- scripts.old/run_certbot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo certbot certonly --webroot -w /home/cv/static -d qwality.fun -d www.qwality.fun
--- scripts.old/test_cert.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Nastavíme pracovní adresář na adresář, ve kterém je umístěn skript
-cd "$(dirname "$0")"
-
-# Změníme cestu na relativní, předpokládáme, že složka .well-known je ve složce /home/cv/static
-echo "$1" > ../static/.well-known/acme-challenge/test-file.txt
-curl http://www.qwality.fun/.well-known/acme-challenge/test-file.txt
