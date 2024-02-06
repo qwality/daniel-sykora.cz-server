69a5095c4c7dbf2c32519cc7ffad5925a1175012
qwality <low.qwality@gmail.com>
2024-02-06 18:41:33 +0100

    mezera

--- cron-maintainer.sh
+++ cron-maintainer.sh
@@ -3,10 +3,10 @@
 status_1=$(qwality-server -q status daniel-sykora.cz)
 status_2=$(qwality-server -q status rails-redmine)
 
-if [[ $status_1 != "OK"]]; then
+if [[ $status_1 != "OK" ]]; then
     qwality-server run danie-sykora.cz
 fi
 
-if [[ $status_2 != "OK"]]; then
+if [[ $status_2 != "OK" ]]; then
     qwality-server run rails-redmine
 fi
