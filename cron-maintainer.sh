761524ba26ea50e6ff33f04aa1bc535fd917627d
qwality <low.qwality@gmail.com>
2024-02-10 13:02:03 +0100

    zmena run na redeploy

--- cron-maintainer.sh
+++ cron-maintainer.sh
@@ -4,9 +4,9 @@ status_1=$(qwality-server -q status daniel-sykora.cz)
 status_2=$(qwality-server -q status rails-redmine)
 
 if [[ $status_1 != "OK" ]]; then
-    qwality-server run danie-sykora.cz
+    qwality-server redeploy danie-sykora.cz
 fi
 
 if [[ $status_2 != "OK" ]]; then
-    qwality-server run rails-redmine
+    qwality-server redeploy rails-redmine
 fi
