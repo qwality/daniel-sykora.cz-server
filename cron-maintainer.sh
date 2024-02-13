38375ac9d2f290fd6c7b9b09b16d16f6c2f6abf0
qwality <low.qwality@gmail.com>
2024-02-13 14:28:24 +0100

    t1

--- cron-maintainer.sh
+++ cron-maintainer.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-source ~/.bashrc
+# source ~/.bashrc
 
-nvm -v
-node -v
-rails -v
+# nvm -v
+# node -v
+# rails -v
 
-# status_1=$(qwality-server -q status daniel-sykora.cz)
-# status_2=$(qwality-server -q status rails-redmine)
+status_1=$(qwality-server -q status daniel-sykora.cz)
+status_2=$(qwality-server -q status rails-redmine)
 
-# if [[ $status_1 != "OK" ]]; then
-#     qwality-server redeploy daniel-sykora.cz
-# else
-#     echo "$(date '+%Y-%m-%d %H:%M:%S') - daniel-sykora.cz is OK"
-# fi
+if [[ $status_1 != "OK" ]]; then
+    qwality-server redeploy daniel-sykora.cz
+else
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - daniel-sykora.cz is OK"
+fi
 
-# if [[ $status_2 != "OK" ]]; then
-#     qwality-server redeploy rails-redmine
-# else
-#     echo "$(date '+%Y-%m-%d %H:%M:%S') - rails-redmine is OK"
-# fi
+if [[ $status_2 != "OK" ]]; then
+    qwality-server redeploy rails-redmine
+else
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - rails-redmine is OK"
+fi
