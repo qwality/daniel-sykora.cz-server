2569a6f4be724bb3db536ffcbf2d12f2e3a84396
qwality <low.qwality@gmail.com>
2024-02-13 16:12:44 +0100

    dsdas

--- cron-maintainer.sh
+++ cron-maintainer.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
 # source ~/.bashrc
-. $HOME/.asdf/asdf.sh
-asdf shell nodejs 21.6.1
-nvm -v
+# . $HOME/.asdf/asdf.sh
+# asdf shell nodejs 21.6.1
+# nvm -v
 node -v
 rails -v
 # asdf -v
 
-# status_1=$(qwality-server -q status daniel-sykora.cz)
-# status_2=$(qwality-server -q status rails-redmine)
+status_1=$(qwality-server -q status daniel-sykora.cz)
+status_2=$(qwality-server -q status rails-redmine)
 
-# if [[ $status_1 != "OK" ]]; then
-#     qwality-server redeploy daniel-sykora.cz
-# else
-#     echo "$(date '+%Y-%m-%d %H:%M:%S') - daniel-sykora.cz is OK"
-# fi
+if [[ $status_1 != "OK" ]]; then
+    qwality-server redeploy daniel-sykora.cz
+else
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - daniel-sykora.cz is OK"
+fi
 
-# if [[ $status_2 != "OK" ]]; then
-#     qwality-server redeploy rails-redmine
-# else
-#     echo "$(date '+%Y-%m-%d %H:%M:%S') - rails-redmine is OK"
-# fi
+if [[ $status_2 != "OK" ]]; then
+    qwality-server redeploy rails-redmine
+else
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - rails-redmine is OK"
+fi
